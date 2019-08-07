30f0c8e201383828cff71dd209a176d7eadf121b
Naty89 <37753984+Naty89@users.noreply.github.com>
2019-08-07 14:23:19 -0400

    Update move_method2.py

--- move_method2.py
+++ move_method2.py
@@ -1,16 +1,12 @@
 def move(str):
-	# l = []
+	l = []
+	for i in str:
+		if i == 'z' or i == 'Z':
+	 		l.append(chr(int(ord(i)) - 25))
+	 	else:
+	 		l.append(chr(int(ord(i))+1))
 
-	return ''.join([ chr(int(ord(x)) - 25) if x=='z' or x == 'Z' else chr(int(ord(x) + 1)) for x in str])
-
-
-	# for i in str:
-	# 	if i == 'z' or i == 'Z':
-	# 		l.append(chr(int(ord(i)) - 25))
-	# 	else:
-	# 		l.append(chr(int(ord(i))+1))
-
-	# return "".join(l)
+	return "".join(l)
 
 print(move("zebra"))	
 
